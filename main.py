8a136830ed4058aba0ea7b724fb8ba07df1890ae
MrValeriano <jpsvalerius@gmail.com>
2025-07-01 12:36:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 print("Olá turma do Python!")
 print("Tudo bem?")
 nome: str = input("Qual é o seu nome?\n> ")
-idade = int(input("Qual é a sua idade?\n> "))
-print(f"Olá, {nome}! Você é {"maior" if idade >= 18 else "menor"} de idade!")
+try:
+    idade = int(input("Qual é a sua idade?\n> "))
+    print(f"Olá, {nome}! Você é {"maior" if idade >= 18 else "menor"} de idade!")
+    if not 0 < idade <= 100: raise Exception
+except ValueError:
+    print("\nErro! A idade deve ser um número")
+except Exception:
+    print("\nErro! Número inválido!")
